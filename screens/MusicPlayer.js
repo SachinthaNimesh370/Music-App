7b37126157a8ed6a9a3196519af9888a0c05a9d2
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-08-14 00:34:00 +0530

    Add assets File

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,13 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const {width,height}=Dimensions.get('window');
 
 const MusicPlayer = () => {
   return (
-    <View style={style.container}>
-        <Ionicons name="heart-outline" size={30}/>
-        <Text>React Native Music Player</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+
+        <View style={style.maincontainer}>
+            {/* image */}
+
+            {/* slider */}
+
+            {/* music controls */}
+            
+        </View>
+
+        <View style={style.bottomcontainer}>
+            <View style={style.bottomIconWrapper}>
+                <TouchableOpacity onPress={()=>{}}>
+                    <Ionicons name="heart-outline" size={30} color="#888888"/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>{}}>
+                    <Ionicons name="repeat" size={30} color="#888888"/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>{}}>
+                    <Ionicons name="share-outline" size={30} color="#888888"/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>{}}>
+                    <Ionicons name="ellipsis-horizontal" size={30} color="#888888"/>
+                </TouchableOpacity>
+            </View>
+            
+        </View>
+
+    </SafeAreaView>
+    
   )
 }
 
@@ -17,10 +49,28 @@ export default MusicPlayer
 const style=StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'#555',
+        backgroundColor:'#222831',
+
+    },
+    maincontainer:{
+        flex:1,
         alignItems:'center',
         justifyContent:'center',
+    },
+    bottomcontainer:{
+        width: width,
+        alignItems:'center',
+        paddingVertical:15,
+        borderTopColor:'#393E46',
+        borderWidth:1,
+
+    },
+    bottomIconWrapper:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        width:'80%'
 
     }
+
 })
 
