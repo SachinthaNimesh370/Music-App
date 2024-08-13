fc909c24a27177c10180e4bcbcc7f1915a973e81
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-08-13 23:51:25 +0530

    Install Vector Icon And Slider

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const MusicPlayer = () => {
   return (
     <View style={style.container}>
+        <Ionicons name="heart-outline" size={30}/>
         <Text>React Native Music Player</Text>
     </View>
   )
